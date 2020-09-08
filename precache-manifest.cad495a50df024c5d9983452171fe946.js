e0eb1a7ac9f9b53ad302a0187e0fc50724861dfe
davincikab <davidnganganjeri079@gmail.com>
2020-09-08 14:40:27 +0300

    Updates

---
+++ precache-manifest.cad495a50df024c5d9983452171fe946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7b20fc2115a09f7c771b320e37b64c",
+    "url": "/davincikab/.github.io/my-home/index.html"
+  },
+  {
+    "revision": "f4249facc659ed6e03af",
+    "url": "/davincikab/.github.io/my-home/static/css/main.0687bc98.chunk.css"
+  },
+  {
+    "revision": "de17181aa7322d6355e3",
+    "url": "/davincikab/.github.io/my-home/static/js/2.f74c87aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/davincikab/.github.io/my-home/static/js/2.f74c87aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4249facc659ed6e03af",
+    "url": "/davincikab/.github.io/my-home/static/js/main.b83fa568.chunk.js"
+  },
+  {
+    "revision": "826e1f0349ab860048bf",
+    "url": "/davincikab/.github.io/my-home/static/js/runtime-main.a2753086.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/davincikab/.github.io/my-home/static/media/logo.5d5d9eef.svg"
+  }
+]);
