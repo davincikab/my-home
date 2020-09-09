0ca950a216ecba45a958ac2932cea1683cc4151b
davincikab <davidnganganjeri079@gmail.com>
2020-09-10 01:19:36 +0300

    Updates

---
+++ precache-manifest.c616cbb407dc8310bf61246119e66e5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38694fd94c102626b9a81329bc5931fb",
+    "url": "/my-home/index.html"
+  },
+  {
+    "revision": "405aba38e06aa39a676b",
+    "url": "/my-home/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "0f6d9682a1a32627af9c",
+    "url": "/my-home/static/css/main.e509b331.chunk.css"
+  },
+  {
+    "revision": "405aba38e06aa39a676b",
+    "url": "/my-home/static/js/2.6c32df98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-home/static/js/2.6c32df98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6d9682a1a32627af9c",
+    "url": "/my-home/static/js/main.27c468ee.chunk.js"
+  },
+  {
+    "revision": "aed9109603a0bb93386c",
+    "url": "/my-home/static/js/runtime-main.b57a1af9.js"
+  },
+  {
+    "revision": "672e8a1a9779d9529c5cbf04b97ff8b2",
+    "url": "/my-home/static/media/children.672e8a1a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-home/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-home/precache-manifest.cea2e80406198e3986c627d72396e313.js"
+  "/my-home/precache-manifest.c616cbb407dc8310bf61246119e66e5e.js"
 );
 
 self.addEventListener('message', (event) => {
