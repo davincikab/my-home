fde229fc40a9edc607eecbb352b210c4495a2100
davincikab <davidnganganjeri079@gmail.com>
2020-09-13 10:14:09 +0300

    updated filter components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
--- src/Component/Map/Map.js
+++ src/Component/Map/Map.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react";
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Popup } from 'react-mapbox-gl';
 
 // firebase
 import {withFirebase } from '../Firebase';
+import './Map.css';
 
 // access token
 const Map = ReactMapboxGl({
@@ -12,6 +13,9 @@ const Map = ReactMapboxGl({
 
 function MapComponent(props) {
     const [homes, setHomes] = useState([]);
+    const [popupData, setPopupData] = useState({});
+    const [zoom, setZoom] = useState(10);
+    const [center, setCenter] = useState([34.9321088,-0.6299662865]);
 
     useEffect(() =>{
         // get the data from firebase
@@ -34,32 +38,153 @@ function MapComponent(props) {
         }
     };
 
-    console.log(homes);
+    const onMapClick = (map, event) => {
+        let features = map.queryRenderedFeatures(event.point, {layers:["homes"]});
+       
+        // get map zoom 
+        let currentZoom = map.getZoom();
+        let currentCenter = map.getCenter();
+
+        if(features[0]) {
+            // get the clicked layer
+            let feature = features[0];
+
+            let activeHome = homes.find(hme => hme.properties.id == feature.properties.id);
+            setPopupData(activeHome);
+        } else {
+            setPopupData({});
+        }
+
+        setZoom(currentZoom);
+        setCenter(currentCenter);
+        
+    }
+
+    const onFeatureClick = (event, home) => {
+        // update the popupdata
+        setPopupData(home);
+        event.originalEvent.stopPropagation();
+
+        console.log(event);
+    }
+
     return (
-        <div>
+        <div className="container">
           <Map
             style='mapbox://styles/mapbox/light-v10'
             containerStyle={{
                 height: '91.3vh',
                 width: '100vw'
             }}
-            center={[34.9321088,-0.6299662865]}
-            zoom={[10]}
+            center={center}
+            zoom={[zoom]}
             onStyleImageMissing={onStyleImageMissing}
+            onClick={onMapClick}
             >
             
             {   
                 homes[0] &&
-                <Layer type="symbol" id="marker" layout={{ 'icon-image': "home"}}>
+                <Layer type="symbol" id="homes" layout={{ 'icon-image': "home"}}>
                   {
                     homes.map(home => (
-                        <Feature key={home.properties.id} coordinates={home.geometry.coordinates} />
+                        <Feature 
+                            key={home.properties.id} 
+                            coordinates={home.geometry.coordinates} 
+                            onClick={e => onFeatureClick(e, home)}
+                        />
                     ))
                   }
                 </Layer>
             }
+
+            {
+                popupData.geometry &&
+                <Popup
+                    coordinates={popupData.geometry.coordinates}
+                    offset={{
+                        'bottom-left': [12, -38],  'bottom': [0, 0], 'bottom-right': [-12, -38]
+                    }}
+                >
+                    <div className="popup-content">
+                        <h1 className="popup-title">{popupData.properties.Name}</h1>
+                        <div className="popup-body">
+                            <p><b>Owner</b> {popupData.properties.Owner}</p>
+                            <p><b>Population</b> {popupData.properties.Population}</p>
+                            <p><b>Tel Phone</b> {popupData.properties.telephone_no}</p>
+                        </div>
+                    </div>
+                </Popup>
+            }
            
             </Map>
+
+            {/* filter tab */}
+            <div className="filter-tab">
+                <div className="section">
+                    <h5 className="section-title">Visualise By</h5>
+
+                    {/*  */}
+                    <div className="visual-section">
+                        <label htmlFor="population" className="filter">
+                            <input 
+                                type="radio"    
+                            /> Population
+                        </label>
+
+                        <label htmlFor="population" className="filter">
+                            <input 
+                                type="radio"    
+                            />Females
+                        </label>
+
+                        <label htmlFor="population" className="filter">
+                            <input 
+                                type="radio"    
+                            /> Male
+                        </label>
+
+                        <label htmlFor="population" className="filter">
+                            <input 
+                                type="radio"    
+                            /> Beds
+                        </label> 
+
+                        <label htmlFor="population" className="filter">
+                            <input 
+                                type="radio"    
+                            /> Population
+                        </label>         
+                    </div>
+                </div>
+                <div className="section">
+                    <h5 className="section-title">Filter</h5>
+                    <div className="filter-section">
+                        <div className="form-group">
+                            <label>Beds</label>
+                            <input type="range" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Male</label>
+                            <input type="range" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Population</label>
+                            <input type="range" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Age Sets</label>
+                            <select className="form-control">
+                                <option name="">10 - 22</option>
+                                <option name="">10 - 22</option>
+                                <option name="">10 - 22</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
